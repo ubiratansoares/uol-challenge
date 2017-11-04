14693dddb9f68c204ebd0fab56ee3c605f140150
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-11-04 00:20:04 -0200

    Fixing wrong declaration

--- app/src/main/java/br/ufs/uolchallenge/HomeActivity.kt
+++ app/src/main/java/br/ufs/uolchallenge/HomeActivity.kt
@@ -2,11 +2,8 @@ package br.ufs.uolchallenge
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import br.ufs.uolchallenge.presentation.NewsFeedViewModel
 
 class HomeActivity : AppCompatActivity() {
-
-    val presenter = NewsFeedViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
